5088f936379d60965cad5c88b66d328e35677ae5
Denis Costa <deniscostadsc@gmail.com>
2012-09-28 09:27:21 -0300

    Arrumando bug. Não chegava na segunda URL.

--- childcrud/urls.py
+++ childcrud/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'(?P<p_app_name>\w+)/(?P<p_model_name>\w+)/(?P<p_id>\d+)/(?P<app_name>\w+)/(?P<model_name>\w+)/$', ajax_list, name='childcrud-list'),
     url(r'(?P<p_app_name>\w+)/(?P<p_model_name>\w+)/(?P<p_id>\d+)/(?P<app_name>\w+)/(?P<model_name>\w+)/(?P<id>\d+)/$', ajax_create_update, name='childcrud-update'),
     url(r'(?P<p_app_name>\w+)/(?P<p_model_name>\w+)/(?P<p_id>\d+)/(?P<app_name>\w+)/(?P<model_name>\w+)/new/$', ajax_create_update, name='childcrud-create'),
-    url(r'(?P<app_name>\w+)/(?P<model_name>\w+)/$', fk_create_update, name='fk-create'),
     url(r'(?P<app_name>\w+)/(?P<model_name>\w+)/(?P<id>\d+)/$', fk_create_update, name='fk-update'),
+    url(r'(?P<app_name>\w+)/(?P<model_name>\w+)/$', fk_create_update, name='fk-create'),
 )
