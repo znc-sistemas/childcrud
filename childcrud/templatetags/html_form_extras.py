4f5175ff0df3225bc83be39df806261974f63ccf
Marcelo Fonseca Tambalo <marcelo.tambalo@nectosystems.com.br>
2017-11-30 15:38:10 -0200

    atualiza código para funcionar no django 1.11

--- childcrud/templatetags/html_form_extras.py
+++ childcrud/templatetags/html_form_extras.py
@@ -6,12 +6,8 @@ register = template.Library()
 @register.simple_tag
 def form_field(field, additional_text="", required=False, label=True):
     t = template.loader.get_template('form_field.html')
-    return t.render(
-        template.Context(
-            {
-                'field': field, 'additional_text': additional_text,
-                'required': required,
-                'label': label
-            }
-        )
-    )
+    return t.render({
+        'field': field, 'additional_text': additional_text,
+        'required': required,
+        'label': label
+    })
